956b30eb1f34b9ba369dc809b464efe4889b2eef
Lucas-FB <40270359+Lucas-FB@users.noreply.github.com>
2024-03-26 10:03:25 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,10 +3,19 @@ from flask import render_template
 from flask import json
 from datetime import datetime
 from urllib.request import urlopen
+from io import BytesIO
 import sqlite3
+import requests
+import matplotlib.pyplot as plt
+import base64
+
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+
+owner = 'Lucas-FB'
+repo = '5MCSI_Metriques'
+token = '${{ secrets.TOKEN }}'
+
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
@@ -37,7 +46,31 @@ def histogramme():
 
 @app.route("/commits/")
 def commits():
-    return render_template("commits.html")
+    try:
+        headers = {'Authorization': f'token {token}'}
+        url = f'https://api.github.com/repos/{owner}/{repo}/commits'
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            commits = response.json()
+            timestamps = [datetime.strptime(commit['commit']['author']['date'], "%Y-%m-%dT%H:%M:%SZ") for commit in commits]
+            # Création d'un histogramme des commits par minute
+            minutes = [timestamp.minute + timestamp.hour * 60 for timestamp in timestamps]
+            bins = max(minutes) - min(minutes) + 1
+            plt.hist(minutes, bins=bins, edgecolor='black', linewidth=1.2)
+            plt.xlabel('Temps écoulé (minutes)')
+            plt.ylabel('Nombre de commits')
+            plt.title('Nombre de commits par minute')
+            # Conversion du graphique en base64 pour l'afficher dans la page HTML
+            img = BytesIO()
+            plt.savefig(img, format='png')
+            img.seek(0)
+            graph_url = base64.b64encode(img.getvalue()).decode()
+            plt.close()
+            return render_template('commits.html', graph_url=graph_url)
+        else:
+            return 'Error fetching commits', 500
+    except Exception as e:
+        return str(e), 500
   
 if __name__ == "__main__":
   app.run(debug=True)
